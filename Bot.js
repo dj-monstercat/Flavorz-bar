88c7f4404545ecba8a73897295ed2cdfbf66b76d
dj-monstercat <jordansim189189@gmail.com>
2015-04-22 20:22:38 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -47,7 +47,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/dj-monstercat/Flavorz-bar/master/Bot.js", function (json) {
+        $.get("https://rawgit.com/dj-monstercat/Flavorz-bar/master/langIndex.json", function (json) {
             var link = trapbot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
